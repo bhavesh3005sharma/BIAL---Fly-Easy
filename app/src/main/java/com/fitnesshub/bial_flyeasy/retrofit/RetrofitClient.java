ab744ffd3dc654c2f746b157e802b164881890e7
Bhavesh Sharma <bhavesh3005sharma@gmail.com>
2021-12-30 00:04:47 +0530

    APIs - JWT Authorisation Added & Fix Prefs Data Saving Bug

--- app/src/main/java/com/fitnesshub/bial_flyeasy/retrofit/RetrofitClient.java
+++ app/src/main/java/com/fitnesshub/bial_flyeasy/retrofit/RetrofitClient.java
@@ -1,5 +1,9 @@
 package com.fitnesshub.bial_flyeasy.retrofit;
 
+import android.content.Context;
+
+import com.fitnesshub.bial_flyeasy.BuildConfig;
+import com.fitnesshub.bial_flyeasy.database.Prefs;
 import com.fitnesshub.bial_flyeasy.utils.HelperClass;
 
 import javax.inject.Singleton;
@@ -7,7 +11,10 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -16,6 +23,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 @InstallIn(SingletonComponent.class)
 public class RetrofitClient {
     private static Retrofit retrofit = null;
+    private static Prefs prefs = null;
 
     @Provides
     @Singleton
@@ -25,13 +33,40 @@ public class RetrofitClient {
 
     @Provides
     @Singleton
-    public static Retrofit getInstance() {
+    public static Prefs getPrefs(@ApplicationContext Context context) {
+        if (prefs == null) prefs = new Prefs(context);
+        return prefs;
+    }
+
+    @Provides
+    @Singleton
+    public static Retrofit getInstance(@ApplicationContext Context context) {
+
         if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+            if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+                logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+                logging.redactHeader("Authorization");
+                logging.redactHeader("Cookie");
+                httpClient.addInterceptor(logging);
+            }
+
+            Retrofit.Builder builder = new Retrofit.Builder()
                     .baseUrl(HelperClass.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .build();
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create());
+
+            AuthenticationInterceptor interceptor =
+                    new AuthenticationInterceptor(getPrefs(context).getToken());
+
+            if (!httpClient.interceptors().contains(interceptor))
+                httpClient.addInterceptor(interceptor);
+
+            builder.client(httpClient.build());
+            return retrofit = builder.build();
+
         }
         return retrofit;
     }
